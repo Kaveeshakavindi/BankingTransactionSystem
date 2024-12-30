9f741612b42799381a75c1b74ee88bb1f49d7bb5
Kaveesha Fernando <kaveeshaf@seylan.lk>
2024-12-30 10:43:29 +0530

    Bank Account methods

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccount {
+    private double balance;
+    private final int id;
+    private final ReentrantLock lock = new ReentrantLock(true);
+
+    public BankAccount(int id, double initialBalance) {
+        this.id = id;
+        this.balance = initialBalance;
+    }
+
+    public int getId() {
+        return id;
+    }
+    //read A & read B & don't block deposit & don't block withdraw
+    public double getBalance() {
+        return balance;
+    }
+    //A -> B & C -> D : can run simultaneously
+    //A -> B & B -> C : B waits until A completes
+    public void deposit (double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void lock(){
+        lock.lock();
+    }
+
+    public void unlock() {
+        lock.unlock();
+    }
 }
--- src/main/java/TransactionSystem.java
+++ src/main/java/TransactionSystem.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TransactionSystem {
+    public Map<Integer, BankAccount> accounts;
+
+    public TransactionSystem(List<BankAccount> accountList) {
+        this.accounts = new HashMap<>();
+        for(BankAccount account : accountList) {
+            accounts.put(account.getId(), account);
+        }
+    }
+
+    public boolean transfer(int fromAccountId, int toAccountId, double amount){
+        BankAccount fromAccount = this.accounts.get(fromAccountId);
+        BankAccount toAccount = this.accounts.get(toAccountId);
+        return true;
+    }
+
+    public void reverseTransaction(int fromAccountId, int toAccountId, double amount){}
+
+    public void printAccountBalances(){}
 }
